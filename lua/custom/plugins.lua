f74c505fd1ad63d54a3cc6e570ccb365258719ac
Panos Sakkos <panos.sakkos@gmail.com>
2024-02-27 21:51:38 +0200

    move nvim-dap-python to custom/plugins

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -17,19 +17,6 @@ local plugins = {
     opts = gitsigns.opts,
   },
   {
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "rcarriga/nvim-dap-ui",
-    },
-    config = function(_, _)
-      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-      require("dap-python").setup(path)
-      require("core.utils").load_mappings "dap_python"
-    end,
-  },
-  {
     "ellisonleao/glow.nvim",
     config = true,
     cmd = "Glow",
