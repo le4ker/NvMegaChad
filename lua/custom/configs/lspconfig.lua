4e2baa18f5e7a26394be7bac61e275d26282c767
Panos Sakkos <panos.sakkos@gmail.com>
2024-03-15 12:53:14 +0200

    remove schema path from yamlls

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -69,7 +69,6 @@ lspconfig.yamlls.setup {
         ["https://panther-community-us-east-1.s3.amazonaws.com/latest/logschema/schema-tests.json"] = "internal/log_analysis/yamlschemas/schemas/**/*_tests.yml",
         ["https://raw.githubusercontent.com/panther-labs/panther_analysis_tool/main/panther_analysis_tool/detection_schemas/analysis_config_schema.json"] = {
           "rules/**",
-          "simple_rules/**",
           "policies/**",
           "queries/**",
         },
