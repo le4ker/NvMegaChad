ed7edc5273628f711c5a497d39969ab05ce146a8
Panos Sakkos <panos.sakkos@gmail.com>
2024-12-21 19:57:20 +0200

    ensure mason plugins are installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,17 @@
 return {
   "williamboman/mason.nvim",
+  config = function(_, opts)
+    require("mason").setup(opts)
+    local mason_registry = require("mason-registry")
+    local ensure_installed = opts.ensure_installed or {}
+
+    -- Ensure all listed tools are installed
+    for _, tool in ipairs(ensure_installed) do
+      local package = mason_registry.get_package(tool)
+      if not package:is_installed() then
+        package:install()
+      end
+    end  end,
   opts = {
     ensure_installed = {
       "bash-language-server",
