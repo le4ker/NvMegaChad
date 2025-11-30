2fb6f8354abb1e089cfa23e0c92d53323e09634e
Panos Sakkos <panos.sakkos@gmail.com>
2025-11-30 19:58:01 +0200

    fix: remove copilot dead code

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,28 +1,12 @@
 ---@type ChadrcConfig
 local M = {}
 
-local function lsp()
-  if rawget(vim, "lsp") then
-    for _, client in ipairs(vim.lsp.get_clients()) do
-      local stbufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
-
-      -- ignore copilot
-      if client.attached_buffers[stbufnr] and client.name ~= "copilot" then
-        return (vim.o.columns > 100 and "%#St_LspStatus#" .. "   LSP ~ " .. client.name .. " ") or "   LSP "
-      end
-    end
-  end
-end
-
 M.ui = {
   tabufline = {
     order = { "treeOffset", "buffers", "tabs" },
   },
   statusline = {
     separator_style = "block",
-    modules = {
-      lsp = lsp,
-    },
   },
 }
 
