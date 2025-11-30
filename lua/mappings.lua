6eb34bd5cca2c99a5332ded71c723c207d3a8b57
Panos Sakkos <panos.sakkos@gmail.com>
2025-11-30 13:03:56 +0200

    feat: improve keymaps with new LSP bindings, toggle options, and terminal escape handling

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,8 +1,12 @@
 local map = vim.keymap.set
 
 -- LSP
-map("n", "[d", vim.diagnostic.goto_prev, { desc = "LSP Go to previous diagnostic" })
-map("n", "]d", vim.diagnostic.goto_next, { desc = "LSP Go to next diagnostic" })
+map("n", "[d", function()
+  vim.diagnostic.jump { count = -1 }
+end, { desc = "LSP Go to previous diagnostic" })
+map("n", "]d", function()
+  vim.diagnostic.jump { count = 1 }
+end, { desc = "LSP Go to next diagnostic" })
 
 -- tabufline
 map("n", "<leader>b", "<cmd>enew<CR>", { desc = "Buffer New" })
@@ -55,7 +59,8 @@ local function close_terminal()
   vim.api.nvim_win_close(win, true)
 end
 
-map("t", "<ESC>", close_terminal, { desc = "Terminal Close terminal" })
+map("t", "<ESC><ESC>", close_terminal, { desc = "Terminal Close terminal" })
+map("t", "<ESC>", "<C-\\><C-n>", { desc = "Terminal Enter normal mode" })
 map("n", "<leader>h", function()
   require("nvchad.term").new { pos = "sp", size = 0.5 }
 end, { desc = "Terminal New horizontal terminal" })
@@ -74,37 +79,47 @@ map("n", "<C-h>", "<C-w>h", { desc = "General Switch Window left" })
 map("n", "<C-l>", "<C-w>l", { desc = "General Switch Window right" })
 map("n", "<C-j>", "<C-w>j", { desc = "General Switch Window down" })
 map("n", "<C-k>", "<C-w>k", { desc = "General Switch Window up" })
-map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "General File Copy whole" })
+map("n", "<leader>Y", "<cmd>%y+<CR>", { desc = "General Copy whole file" })
 map("n", "<S-u>", "<C-r>", { desc = "General Redo" })
 map("n", "<C-d>", "<C-d>zz", { desc = "General Move half page down and center" })
 map("n", "<C-u>", "<C-u>zz", { desc = "General Move half page up and center" })
 map("n", "<C-f>", "<C-f>zz", { desc = "General Move half down and center" })
 map("n", "<C-b>", "<C-b>zz", { desc = "General Move half up and center" })
 map("n", "<leader>s", "<cmd> w <CR>", { desc = "General Save file" })
+map("n", "<leader>q", "<cmd>q<CR>", { desc = "General Quit" })
 map("n", "<leader>v", "<cmd> vsplit <CR>", { desc = "General Vertical split" })
 map("n", "<leader>lu", "<cmd> Lazy update <CR>", { desc = "General Update Lazy plugins" })
 map("n", "<leader>pr", "<cmd> MarkdownPreviewToggle <CR>", { desc = "General Preview Markdown file" })
-map("n", "<leader>fmt", function()
+map("n", "<leader>tf", function()
   vim.g.format_on_save = not vim.g.format_on_save
-  if vim.g.format_on_save then
-    print "Format on save enabled"
-  else
-    print "Format on save disabled"
-  end
+  print(vim.g.format_on_save and "Format on save enabled" or "Format on save disabled")
 end, { desc = "General Toggle format on save" })
 
 -- DAP
 map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "DAP Add breakpoint at line" })
-map("n", "<leader>dso", "<cmd> DapStepOver <CR>", { desc = "DAP Step over" })
+map("n", "<leader>dov", "<cmd> DapStepOver <CR>", { desc = "DAP Step over" })
 map("n", "<leader>dsi", "<cmd> DapStepIn <CR>", { desc = "DAP Step in" })
 map("n", "<leader>dc", "<cmd> DapContinue <CR>", { desc = "DAP Continue" })
 map("n", "<leader>dt", "<cmd> DapTerminate <CR>", { desc = "DAP Terminate" })
+map("n", "<leader>dou", "<cmd> DapStepOut <CR>", { desc = "DAP Step out" })
 map("n", "<leader>dgt", function()
   require("dap-go").debug_test()
 end, { desc = "DAP Debug Go test" })
-map("n", "<leader>dglt", function()
+map("n", "<leader>dgl", function()
   require("dap-go").debug_last_test()
 end, { desc = "DAP Debug last Go test" })
 map("n", "<leader>dpt", function()
   require("dap-python").test_method()
 end, { desc = "DAP Debug Python test" })
+
+-- LSP
+map("n", "gd", vim.lsp.buf.definition, { desc = "LSP Go to definition" })
+map("n", "gr", vim.lsp.buf.references, { desc = "LSP References" })
+map("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
+map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP Rename" })
+map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code action" })
+
+-- Toggle relative line numbers
+map("n", "<leader>tn", function()
+  vim.wo.relativenumber = not vim.wo.relativenumber
+end, { desc = "General Toggle relative numbers" })
