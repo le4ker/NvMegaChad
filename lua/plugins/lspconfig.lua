694b7235d60e2c859431f583461eb990c4e02963
Panos Sakkos <panos.sakkos@gmail.com>
2025-12-13 13:25:08 +0200

    fix(lspconfig): add lazy=false to ensure LSP attaches on startup

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,7 @@
-return   {
-    "neovim/nvim-lspconfig",
-    config = function()
+return {
+  "neovim/nvim-lspconfig",
+  lazy = false,
+  config = function()
       require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
