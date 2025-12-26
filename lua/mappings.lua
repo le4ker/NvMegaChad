1d52b201f7138bdf6dbfec23e7debd2ebbb78771
Panos Sakkos <panos.sakkos@gmail.com>
2025-12-26 22:17:30 +0200

    style: add silent = true to command mappings
    Suppresses command display in the command line for cleaner UI

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,7 +9,7 @@ map("n", "]d", function()
 end, { desc = "LSP Go to next diagnostic" })
 
 -- tabufline
-map("n", "<leader>b", "<cmd>enew<CR>", { desc = "Buffer New" })
+map("n", "<leader>b", "<cmd>enew<CR>", { desc = "Buffer New", silent = true })
 
 map("n", "<tab>", function()
   require("nvchad.tabufline").next()
@@ -36,16 +36,16 @@ map(
 )
 
 -- NvimTree
-map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "NvimTree Toggle window" })
-map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "NvimTree Focus window" })
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "NvimTree Toggle window", silent = true })
+map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "NvimTree Focus window", silent = true })
 
 -- Telescope
-map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "Telescope Live grep" })
-map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Telescope Find buffers" })
-map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "Telescope Git commits" })
-map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "Telescope Git status" })
-map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "Telescope Pick hidden term" })
-map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Telescope Find files" })
+map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "Telescope Live grep", silent = true })
+map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Telescope Find buffers", silent = true })
+map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "Telescope Git commits", silent = true })
+map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "Telescope Git status", silent = true })
+map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "Telescope Pick hidden term", silent = true })
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Telescope Find files", silent = true })
 map(
   "n",
   "<leader>fa",
@@ -65,42 +65,42 @@ map("n", "<leader>h", function()
 end, { desc = "Terminal New horizontal terminal" })
 
 -- NvChad
-map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", { desc = "NvChad Toggle NvCheatsheet" })
-map("n", "<leader>th", "<cmd>Telescope themes<CR>", { desc = "NvChad Show themes" })
+map("n", "<leader>ch", "<cmd>NvCheatsheet<CR>", { desc = "NvChad Toggle NvCheatsheet", silent = true })
+map("n", "<leader>th", "<cmd>Telescope themes<CR>", { desc = "NvChad Show themes", silent = true })
 
 -- General
 map("i", "<C-h>", "<Left>", { desc = "General Move Left" })
 map("i", "<C-l>", "<Right>", { desc = "General Move Right" })
 map("i", "<C-j>", "<Down>", { desc = "General Move Down" })
 map("i", "<C-k>", "<Up>", { desc = "General Move Up" })
-map("n", "<Esc>", "<cmd>noh<CR>", { desc = "General Clear highlights" })
+map("n", "<Esc>", "<cmd>noh<CR>", { desc = "General Clear highlights", silent = true })
 map("n", "<C-h>", "<C-w>h", { desc = "General Switch Window left" })
 map("n", "<C-l>", "<C-w>l", { desc = "General Switch Window right" })
 map("n", "<C-j>", "<C-w>j", { desc = "General Switch Window down" })
 map("n", "<C-k>", "<C-w>k", { desc = "General Switch Window up" })
-map("n", "<leader>Y", "<cmd>%y+<CR>", { desc = "General Copy whole file" })
+map("n", "<leader>Y", "<cmd>%y+<CR>", { desc = "General Copy whole file", silent = true })
 map("n", "<S-u>", "<C-r>", { desc = "General Redo" })
 map("n", "<C-d>", "<C-d>zz", { desc = "General Move half page down and center" })
 map("n", "<C-u>", "<C-u>zz", { desc = "General Move half page up and center" })
 map("n", "<C-f>", "<C-f>zz", { desc = "General Move full page down and center" })
 map("n", "<C-b>", "<C-b>zz", { desc = "General Move full page up and center" })
-map("n", "<leader>s", "<cmd>w<CR>", { desc = "General Save file" })
-map("n", "<leader>q", "<cmd>q<CR>", { desc = "General Quit" })
-map("n", "<leader>v", "<cmd>vsplit<CR>", { desc = "General Vertical Split" })
-map("n", "<leader>lu", "<cmd>Lazy update<CR>", { desc = "General Update Lazy plugins" })
-map("n", "<leader>pr", "<cmd>MarkdownPreviewToggle<CR>", { desc = "General Preview Markdown file" })
+map("n", "<leader>s", "<cmd>w<CR>", { desc = "General Save file", silent = true })
+map("n", "<leader>q", "<cmd>q<CR>", { desc = "General Quit", silent = true })
+map("n", "<leader>v", "<cmd>vsplit<CR>", { desc = "General Vertical Split", silent = true })
+map("n", "<leader>lu", "<cmd>Lazy update<CR>", { desc = "General Update Lazy plugins", silent = true })
+map("n", "<leader>pr", "<cmd>MarkdownPreviewToggle<CR>", { desc = "General Preview Markdown file", silent = true })
 map("n", "<leader>tf", function()
   vim.g.format_on_save = not vim.g.format_on_save
   vim.notify(vim.g.format_on_save and "Format on save enabled" or "Format on save disabled")
 end, { desc = "General Toggle format on save" })
 
 -- DAP
-map("n", "<leader>db", "<cmd>DapToggleBreakpoint<CR>", { desc = "DAP Add breakpoint at line" })
-map("n", "<leader>dn", "<cmd>DapStepOver<CR>", { desc = "DAP Step over (next)" })
-map("n", "<leader>di", "<cmd>DapStepIn<CR>", { desc = "DAP Step in" })
-map("n", "<leader>dc", "<cmd>DapContinue<CR>", { desc = "DAP Continue" })
-map("n", "<leader>dt", "<cmd>DapTerminate<CR>", { desc = "DAP Terminate" })
-map("n", "<leader>do", "<cmd>DapStepOut<CR>", { desc = "DAP Step out" })
+map("n", "<leader>db", "<cmd>DapToggleBreakpoint<CR>", { desc = "DAP Add breakpoint at line", silent = true })
+map("n", "<leader>dn", "<cmd>DapStepOver<CR>", { desc = "DAP Step over (next)", silent = true })
+map("n", "<leader>di", "<cmd>DapStepIn<CR>", { desc = "DAP Step in", silent = true })
+map("n", "<leader>dc", "<cmd>DapContinue<CR>", { desc = "DAP Continue", silent = true })
+map("n", "<leader>dt", "<cmd>DapTerminate<CR>", { desc = "DAP Terminate", silent = true })
+map("n", "<leader>do", "<cmd>DapStepOut<CR>", { desc = "DAP Step out", silent = true })
 map("n", "<leader>dgt", function()
   require("dap-go").debug_test()
 end, { desc = "DAP Debug Go test" })
