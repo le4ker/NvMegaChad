97a79f18a89a77a0495f26402a311d3114bd5de9
Panos Sakkos <panos.sakkos@gmail.com>
2026-09-20 19:32:42 +0300

    feat(completion): configure native LSP completion UI

--- lua/configs/completion.lua
+++ lua/configs/completion.lua
@@ -1,10 +1,21 @@
 -- Native LSP completion (Neovim 0.11+)
+
+vim.opt.completeopt = {
+  "menu",
+  "menuone",
+  "noselect",
+  "popup",
+}
+
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("NativeLspCompletion", { clear = true }),
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
+
     if client and client:supports_method "textDocument/completion" then
-      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+      vim.lsp.completion.enable(true, client.id, args.buf, {
+        autotrigger = true,
+      })
     end
   end,
 })
